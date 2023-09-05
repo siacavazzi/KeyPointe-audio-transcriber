5743947a054d7aa1bf5dae30aebc20ce9042c671
Sam <siacavazzi@gmail.com>
2023-09-05 12:02:39 -0400

    ignored pycache

--- lib/conversation.py
+++ lib/conversation.py
@@ -1,15 +1,33 @@
 
-class Conversation:
-    def __init__(self):
-        pass
+from lib import CONN, CURSOR
+from datetime import datetime
 
-    def create_table(self):
+
+class Conversation:
+    def __init__(self, conversation_id=None):
+        self.conversation_id = conversation_id
+  
+    @classmethod
+    def create_table(cls):
         query = """
         CREATE TABLE IF NOT EXISTS conversations (
         id INTEGER PRIMARY KEY,
         time TIMESTAMP,
         convo_id INTEGER,
-        content TEXT,
+        content TEXT
         )
         """
+        CURSOR.execute(query)
+
+    def add_row(self, content):
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        query = """
+        INSERT INTO conversations
+        (time, convo_id, content) VALUES (?,?,?)
+        """
+        CURSOR.execute(query, [current_time, self.conversation_id, content])
+        CONN.commit()
+
+
+
         
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from lib import CONN, CURSOR
 from lib.conversation import Conversation
 
 mic = WhisperMic(english=True, model="small")
+Conversation.create_table()
+
+current_convo = Conversation()
 while True:
     result = mic.listen()
     print(result)
+    current_convo.add_row(result)
