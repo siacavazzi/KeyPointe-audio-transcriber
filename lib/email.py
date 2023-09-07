864554de4f0c71aaaaf19b2061e56a5c04b73e7d
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-09-07 15:50:05 -0400

    email

--- lib/email.py
+++ lib/email.py
@@ -1,19 +1,20 @@
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
-# Load your credentials file (downloaded from the Google Cloud Console)
+
 credentials = service_account.Credentials.from_service_account_file(
     'path/to/your/credentials.json',
     scopes=['https://www.googleapis.com/auth/gmail.send']
 )
 
-# Build the Gmail API client
 service = build('gmail', 'v1', credentials=credentials)
 
 
 def send_email(sender, to, subject, message_text, attachment_path=None):
     message = create_message(sender, to, subject, message_text, attachment_path)
     send_message(service, 'me', message)
+
+
 
 def create_message(sender, to, subject, message_text, attachment_path=None):
     from email.mime.multipart import MIMEMultipart
@@ -25,11 +26,9 @@ def create_message(sender, to, subject, message_text, attachment_path=None):
     message = MIMEMultipart()
     message['to'] = to
     message['subject'] = subject
-
-    # Create and attach the message text
     message.attach(MIMEText(message_text, 'plain'))
 
-    # Attach the file if specified
+    #attach a file...
     if attachment_path:
         part = MIMEBase('application', 'octet-stream')
         part.set_payload(open(attachment_path, 'rb').read())
@@ -55,7 +54,7 @@ send_email(
     to='recipient@example.com',
     subject='Conversation Share',
     message_text='Here is the conversation you requested to share.',
-    attachment_path='path/to/conversation.pdf'  # Optional attachment
+    attachment_path='path/to/conversation.pdf'  # Optional 
 )
 
 
