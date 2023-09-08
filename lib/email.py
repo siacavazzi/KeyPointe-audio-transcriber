5afe7dbc519ace89bd05c61c441d93e3155bc6b6
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-09-07 22:17:02 -0400

    email finished: just need to test

--- lib/email.py
+++ lib/email.py
@@ -1,60 +1,71 @@
+from conversation import Conversation
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+from bs4 import BeautifulSoup
+from docx import Document
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import base64
+import os
 
+class EmailSender:
+    def __init__(self):
+        self.credentials = service_account.Credentials.from_service_account_file(
+            '/Users/tiffanypretlow/Development/code/phase-3/phase-3-project/audio_transcriber/tensile-runway-398315-79d1699d24be.json',
+            scopes=['https://www.googleapis.com/auth/gmail.send']
+        )
+        self.service = build('gmail', 'v1', credentials=self.credentials)
 
-credentials = service_account.Credentials.from_service_account_file(
-    'path/to/your/credentials.json',
-    scopes=['https://www.googleapis.com/auth/gmail.send']
-)
+    def send_email(self, sender, to, subject, message_text, attachment_path=None, html_content=None):
+        message = self.create_message(sender, to, subject, message_text, attachment_path, html_content)
+        return self.send_message(self.service, 'me', message)
 
-service = build('gmail', 'v1', credentials=credentials)
+    def create_message(self, sender, to, subject, message_text, attachment_path=None, html_content=None):
+        message = MIMEMultipart()
+        message['to'] = to
+        message['subject'] = subject
+        message.attach(MIMEText(message_text, 'plain'))
 
+        if html_content:
+            html_part = MIMEText(html_content, 'html')
+            message.attach(html_part)
 
-def send_email(sender, to, subject, message_text, attachment_path=None):
-    message = create_message(sender, to, subject, message_text, attachment_path)
-    send_message(service, 'me', message)
+        if attachment_path:
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(open(attachment_path, 'rb').read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', f'attachment; filename="{os.path.basename(attachment_path)}"')
+            message.attach(part)
 
+        return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
 
+    def send_message(self, service, user_id, message):
+        try:
+            message = (service.users().messages().send(userId=user_id, body=message).execute())
+            print('Message Id: %s' % message['id'])
+            return message
+        except Exception as e:
+            print('An error occurred: %s' % e)
+            return None
 
-def create_message(sender, to, subject, message_text, attachment_path=None):
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-    from email.mime.base import MIMEBase
-    from email import encoders
-    import base64
+    def get_latest_conversation_id(self):
+        return Conversation.get_highest_id()
 
-    message = MIMEMultipart()
-    message['to'] = to
-    message['subject'] = subject
-    message.attach(MIMEText(message_text, 'plain'))
+    def get_sender_email(self):
+        return input("Enter sender email: ")
 
-    #attach a file...
-    if attachment_path:
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload(open(attachment_path, 'rb').read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', f'attachment; filename="{attachment_path}"')
-        message.attach(part)
+    def get_receiver_email(self):
+        return input("Enter receiver email: ")
 
-    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+    def send_transcript_summary(self):
+        id = self.get_latest_conversation_id()
+        sender_email = self.get_sender_email()
+        receiver_email = self.get_receiver_email()
+        html_content = Conversation.export(id, raw_html=True)
+        self.send_email(sender_email, receiver_email, "Transcript Summary", None, None, html_content)
 
-def send_message(service, user_id, message):
-    try:
-        message = (service.users().messages().send(userId=user_id, body=message)
-                   .execute())
-        print('Message Id: %s' % message['id'])
-        return message
-    except Exception as e:
-        print('An error occurred: %s' % e)
-
-
-
-send_email(
-    sender='your_email@gmail.com',
-    to='recipient@example.com',
-    subject='Conversation Share',
-    message_text='Here is the conversation you requested to share.',
-    attachment_path='path/to/conversation.pdf'  # Optional 
-)
-
-
+if __name__ == "__main__":
+    email_sender = EmailSender()
+    email_sender.send_transcript_summary()
